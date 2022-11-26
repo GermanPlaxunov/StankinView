7616e681d011c171199d4f548fca4207c0b4cc8d
snipe <snipe9029quant@gmail.com>
2022-11-26 13:09:28 +0300

    develop 0.1 --> submodules added.

---
+++ stankin-view-service/src/main/java/com/proggersofluck/stankinviewservice/StankinViewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.proggersofluck.stankinviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StankinViewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StankinViewServiceApplication.class, args);
+    }
+
+}
---
+++ stankin-view-service/src/test/java/com/proggersofluck/stankinviewservice/StankinViewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proggersofluck.stankinviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StankinViewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
